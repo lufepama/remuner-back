a2c515bd923d23437978eb0179d9b34dcd26b9f3
lufepama <lufepama31@gmail.com>
2023-09-12 22:41:23 +0200

    add cors

--- main.py
+++ main.py
@@ -6,9 +6,21 @@ from routers.integrations import router as integrations_router
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from sqlalchemy.ext.declarative import declarative_base
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 Base = declarative_base()
+
+#Add clients
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users_router, prefix="/users")
 app.include_router(teams_router, prefix="/teams")
